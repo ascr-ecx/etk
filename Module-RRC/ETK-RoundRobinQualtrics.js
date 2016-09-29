afa3dfd7960265bd35c79688d40c0c65d3d3d266
Terry Turton <tlturton@cat.utexas.edu>
2016-09-29 13:17:17 -0500

    Updates to Round Robin Comparison
    Add boolean AllOneSide check as flag for possible bad participants;
    explicitly next the image pair container inside the Qualtrics question
    container; use image base URL and list of image names.  Update Survey
    Flow png and README accordingly.

--- Module-RRC/ETK-RoundRobinQualtrics.js
+++ Module-RRC/ETK-RoundRobinQualtrics.js
@@ -18,51 +18,78 @@ The prefixes and code for the Embedded Data can also be changed as needed
 to fit the specific variable names defined by the user.  
 */
 
-// Image names/URLs will need to be updated by user.
-var imgs = [ 
-"http://146.6.82.10/TestImages/Image00.png",
-"http://146.6.82.10/TestImages/Image01.png",
-"http://146.6.82.10/TestImages/Image02.png",
-"http://146.6.82.10/TestImages/Image03.png",
-"http://146.6.82.10/TestImages/Image04.png"
+// List of Test images.  Baseline is first image by default.
+// Baseline will be compared to itself and each of the Test images.
+var imgURL = "http://146.6.82.10/TestImages/";
+var baseNames = [ 
+"Image00",
+"Image01",
+"Image02",
+"Image03",
+"Image04"
 ];
-var imgNames = ["Image00", "Image01", "Image02", "Image03", "Image04"];
+var imgs = [];
+var imgNames = [];
 
 // Add validation pair here if used.
 // Set valsExists to false if no validation pair used.
-valsExists = true;
-valsIndex = 0;
-var vals = [ 
-"http://146.6.82.10/TestImages/Validation0.png",
-"http://146.6.82.10/TestImages/Validation1.png"
+var valExists = true;
+var valIndex = 0;
+var valNames = [
+"Validation0", 
+"Validation1"
 ];
-var valsNames = ["Val00", "Val01"];
 	
-var container = document.getElementById('imageContainer');
+// Create containers, attach global container to Qualtrics Question Container
+var imgContainer = document.getElementById('imageContainer');
+var glbContainer =  this.getQuestionContainer();
 var quesText = document.getElementById('myQuesText')
+imgContainer.className = "pairBox";
+
+// Create buttons with needed attributes and text
 var leftBtn = document.createElement("BUTTON");
-var leftText = document.createTextNode("LEFT");
-leftBtn.appendChild(leftText);
 var rightBtn = document.createElement("BUTTON");
-var rightText = document.createTextNode("RIGHT");
-rightBtn.appendChild(rightText);
 var nextBtn = document.createElement("BUTTON");
 var nextText = document.createTextNode("NEXT Image Pair");
-nextBtn.appendChild(nextText);
+var leftText = document.createTextNode("LEFT");
+var rightText = document.createTextNode("RIGHT");
 
+nextBtn.className = "nextButton";
 leftBtn.className = "choiceButton leftButton";
 rightBtn.className = "choiceButton rightButton";
-nextBtn.className = "nextButton";
+leftBtn.setAttribute = ("type", "radio");
+rightBtn.setAttribute = ("type", "radio");
+nextBtn.appendChild(nextText);
+leftBtn.appendChild(leftText);
+rightBtn.appendChild(rightText);
 
 var imgPairs = [];
 var imgPairsIndex = 0;  
 var imgPairNames = [];
 var didflip = false;
 var that;
+
+// Create variables for write-out to Qualtrics Embedded Data var choicePre = "c";
 var choicePre = "c";
 var choiceEmbData;
 var namePre = "n";
 var nameEmbData;
+var currentChoice;
+
+// Create variables to check if participant only selects one button 
+var allLeft = true;
+var allRight = true;
+var nameAllOneSide = "AllOneSide";
+
+function preloadImages(arr){ // preload imaegs
+    var newimages=[]
+	//force arr parameter to always be an array
+    for (var i=0; i<arr.length; i++){
+    var arr=(typeof arr!="object")? [arr] : arr 
+        newimages[i]=new Image()
+        newimages[i].src=arr[i]
+    }
+}
 
 function shuffle(array) {  // Fisher-Yates Shuffle from stackoverflow
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -72,7 +99,6 @@ function shuffle(array) {  // Fisher-Yates Shuffle from stackoverflow
      // Pick a remaining element...
      randomIndex = Math.floor(Math.random() * currentIndex); 
      currentIndex -= 1;
-
      // And swap it with the current element.
      temporaryValue = array[currentIndex];
      array[currentIndex] = array[randomIndex];
@@ -81,7 +107,7 @@ function shuffle(array) {  // Fisher-Yates Shuffle from stackoverflow
   return array;
 }
 
-function show2Images(container, imgA, imgB) { 
+function show2Images(outContainer, inContainer, imgA, imgB) {  
 	var img;
 	var docFrag = document.createDocumentFragment();
 	var someSpace = document.createTextNode("  ");
@@ -99,19 +125,15 @@ function show2Images(container, imgA, imgB) {
 		docFrag.appendChild(someSpace);
 	    docFrag.appendChild(img=document.createElement('img')).src = imgA;
 	}
-	container.appendChild(docFrag);
-	container.appendChild(document.createElement('br'));
-	container.appendChild(leftBtn);
-	container.appendChild(rightBtn);
-	container.appendChild(document.createElement('br'));
-	container.appendChild(document.createElement('br'));
-	container.appendChild(nextBtn);
+	inContainer.appendChild(docFrag);
+	inContainer.appendChild(document.createElement('br'));
+	inContainer.appendChild(leftBtn);
+	inContainer.appendChild(rightBtn);
+	inContainer.appendChild(document.createElement('br'));
+	inContainer.appendChild(nextBtn);
+	
+	outContainer.appendChild(inContainer);
 	return flipped;
-}
-
-function createImagePairing (firstImageNum, secondImageNum) {
-	imgPairs.push ({iA: firstImageNum, iB: secondImageNum, 
-	                 iAname: imgNames[firstImageNum], iBname: imgNames[secondImageNum]});
 }
 
 function clearContainer(elementID) {
@@ -119,90 +141,100 @@ function clearContainer(elementID) {
 		elementID.removeChild(elementID.firstChild)}
 }
 
-function endStudy(container) {
+function endStudy(inContainer) {
 	var endTextTitle = document.createTextNode("Please click on SUBMIT to record your choices.");
 	var docFrag = document.createDocumentFragment();
 	
 	quesText.style.display = "none";	
-	container.style.backgroundColor = "#CC5500";
-	container.style.fontSize="200%";
-	container.style.fontWeight="bold" ;
+	inContainer.style.backgroundColor = "#CC5500";
+	inContainer.style.fontSize="200%";
+	inContainer.style.fontWeight="bold" ;
 	docFrag.appendChild(endTextTitle); 
-	container.appendChild(docFrag);
+	inContainer.appendChild(docFrag);
 	that.showNextButton();
 }
 
-// Start Round Robin
+// Start Round Robin Comparison
+
 that = this;
 that.hideNextButton();
-clearContainer(container);
+
+// Build the image arrays from the URL and names; preload images
+// Create an array of indices, shuffle that and 
+// always compare the two images with same index.
+for (i=0; i<baseNames.length; i++) {
+	imgs[i] = imgURL + baseNames[i];
+	imgNames[i] = baseNames[i];
+}
 
 // Use an array of objects to determine which images are to be shown.
 for (i= 0; i<imgs.length; i++) { // build the pairs 
 	for (j = i+1; j<imgs.length; j++) {
-		createImagePairing (i, j);
+		imgPairs.push ({iA: i, iB: j, iAname: imgNames[i], iBname: imgNames[j]});
 	}
 }
 
-// If there is a validation pair, add those to the imgPairs to get 
-// randomly shuffled.
-if (valsExists) {
-	valsIndex = imgs.length;
-	imgs.push (vals[0]);
-	imgs.push (vals[1]);
-	imgPairs.push ({iA: valsIndex, iB: valsIndex+1, 
-	               iAname: valsNames[0], iBname: valsNames[1]});
+// If there is a validation pair, add that pair to the imgPairs so it get 
+// randomly shuffled into the list of image pairs.
+if (valExists) {
+	valIndex = imgs.length;
+	imgs.push (imgURL + valNames[0]);
+	imgs.push (imgURL + valNames[1]);
+	imgPairs.push ({iA: valIndex, iB: valIndex+1, 
+	               iAname: valNames[0], iBname: valNames[1]});
 }
-
+preloadImages(imgs);
+clearContainer(imgContainer);
 shuffle(imgPairs);
 
 // Showing initial image pair
 nextBtn.disabled = true;
-didflip = show2Images(document.getElementById('imageContainer'), imgs[imgPairs[0].iA], imgs[imgPairs[0].iB]);
+didflip = show2Images(glbContainer, imgContainer, imgs[imgPairs[0].iA], imgs[imgPairs[0].iB]);
 imgPairNames[imgPairsIndex] = imgPairs[imgPairsIndex].iAname + "-" + imgPairs[imgPairsIndex].iBname;
-if (didflip) {
-	imgPairNames[imgPairsIndex] = imgPairs[imgPairsIndex].iBname + "-" + imgPairs[imgPairsIndex].iAname;
-}
 	
 // Click "Next" button to see next image pair
 nextBtn.addEventListener('click', function() {
-	clearContainer(container);
+	clearContainer(imgContainer);
 	leftBtn.style.backgroundColor = "burlywood";
 	rightBtn.style.backgroundColor = "burlywood";	
 	nextBtn.style.backgroundColor = "burlywood";
 	nextBtn.disabled = true;
 	imgPairsIndex++;
 	if (imgPairsIndex < imgPairs.length) {
-		didflip = show2Images(container, imgs[imgPairs[imgPairsIndex].iA], imgs[imgPairs[imgPairsIndex].iB]);
+		didflip = show2Images(glbContainer, imgContainer, imgs[imgPairs[imgPairsIndex].iA], imgs[imgPairs[imgPairsIndex].iB]);
 		imgPairNames[imgPairsIndex] = imgPairs[imgPairsIndex].iAname + "-" + imgPairs[imgPairsIndex].iBname;
-		if (didflip) {
-			imgPairNames[imgPairsIndex] = imgPairs[imgPairsIndex].iBname + "-" + imgPairs[imgPairsIndex].iAname;
-		}
 	} else {
-		endStudy(container);
+		endStudy(imgContainer);
+		Qualtrics.SurveyEngine.setEmbeddedData(nameAllOneSide, allLeft||allRight);
 	}
 });
 
 leftBtn.addEventListener('click', function(){
  // Note choice for Left Button 
-	leftBtn.style.backgroundColor = "red";
+	leftBtn.style.backgroundColor = "white";
 	rightBtn.style.backgroundColor = "burlywood";
 	nextBtn.style.backgroundColor = "GreenYellow";
 	nextBtn.disabled = false;
+	allRight = false;
 	choiceEmbData = choicePre + imgPairsIndex;
-	Qualtrics.SurveyEngine.setEmbeddedData(choiceEmbData, 1);
 	nameEmbData = namePre + imgPairsIndex;
+	currentChoice = 1;  // 1 = First/Left mage chosen
+	if (didflip) {currentChoice = 2;}  // Flip if needed
+	Qualtrics.SurveyEngine.setEmbeddedData(choiceEmbData, currentChoice);
 	Qualtrics.SurveyEngine.setEmbeddedData(nameEmbData, imgPairNames[imgPairsIndex]);
  });
  rightBtn.addEventListener('click', function(){
  // Note choice for Right Button
-	rightBtn.style.backgroundColor = "red";
+	rightBtn.style.backgroundColor = "white";
 	leftBtn.style.backgroundColor = "burlywood";
 	nextBtn.style.backgroundColor = "GreenYellow";
 	nextBtn.disabled = false;
+	allLeft = false;
 	choiceEmbData = choicePre + imgPairsIndex;
-	Qualtrics.SurveyEngine.setEmbeddedData(choiceEmbData, 2);
 	nameEmbData = namePre + imgPairsIndex;
+	currentChoice = 2;   // 2 = Right/Second image chosen
+	if (didflip) {currentChoice = 1;}   // Flip if needed
+	Qualtrics.SurveyEngine.setEmbeddedData(choiceEmbData, currentChoice);
 	Qualtrics.SurveyEngine.setEmbeddedData(nameEmbData, imgPairNames[imgPairsIndex]);
  });
  
